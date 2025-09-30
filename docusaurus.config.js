d2ae2309baeacb4ff1d6ad51581849a0502cdadc
Joe Lu <joel@PHUS.CORP>
2025-09-30 16:31:13 -0700

    change social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/front_cover.png',
       navbar: {
         title: 'Zerova',
         logo: {
